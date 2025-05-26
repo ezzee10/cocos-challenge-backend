9bac2986279b3e6d6e4db8d78102cb183c58c78c
Ezequiel <ezequiel.colombano@gmail.com>
2025-05-25 22:21:18 -0300

    refactor: config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderModule } from './orders/order.module';
-
-import { UserEntity } from './users/infrastructure/database/entities/user.entity';
-import { MarketDataEntity } from './market/infrastructure/database/entities/market-data.entity';
-import { InstrumentEntity } from './instruments/infrastructure/database/entities/instrument.entity';
-import { OrderEntity } from './orders/infrastructure/database/entities/order.entity';
-
 import { MarketModule } from './market/market.module';
 import { InstrumentModule } from './instruments/instrument.module';
 import { PortfolioModule } from './portfolio/portfolio.module';
 import { UserModule } from './users/user.module';
+
+import { getTypeOrmConfig } from './configs/orm.config';
 
 @Module({
 	imports: [
@@ -20,22 +17,10 @@ import { UserModule } from './users/user.module';
 			envFilePath: `.env.${process.env.NODE_ENV || 'local'}`,
 		}),
 		TypeOrmModule.forRootAsync({
-			useFactory: () => ({
-				type: 'postgres',
-				host: process.env.DB_HOST,
-				port: parseInt(process.env.DB_PORT || '5432', 10),
-				username: process.env.DB_USERNAME,
-				password: process.env.DB_PASSWORD,
-				database: process.env.DB_NAME,
-				entities: [
-					InstrumentEntity,
-					UserEntity,
-					MarketDataEntity,
-					OrderEntity,
-				],
-				synchronize: false,
-				ssl: process.env.DB_SSL === 'true',
-			}),
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) =>
+				getTypeOrmConfig(configService),
 		}),
 		OrderModule,
 		MarketModule,
